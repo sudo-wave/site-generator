a6ddeaf370c61f38865bd07a31e114eddad770c5
Irvin <irvin.l@berkeley.edu>
2024-08-04 18:13:54 -0700

    reformating workspace

--- src/main.py
+++ src/main.py
@@ -1,2 +1,24 @@
-def check_text_type(text_type, text_types):
-    return text_type in text_types
+import os
+
+# Make sure static and public directories exist
+static_dir = "../static/"
+images_dir = "../static/images/"
+public_dir = "../public/"
+
+
+def check_static_folder(dir):
+    if not os.path.exists(dir):
+        raise FileNotFoundError("Static folder not found")
+
+
+def check_images_folder(dir):
+    if not os.path.exists(dir):
+        raise FileNotFoundError("Static folder not found")
+
+
+def check_public_folder(dir):
+    if not os.path.exists(dir):
+        raise FileNotFoundError("Public folder not found")
+
+
+# TODO: write a recurisve function that copies all contents from static to public
